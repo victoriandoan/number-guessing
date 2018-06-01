a93729c3b0b3ff4ef9dde6a60a52498a1d88419a
Victoria <39710898+victoriandoan@users.noreply.github.com>
2018-05-31 22:59:33 -0500

    Add files via upload
    Add what the number was if the user hits Game Over.
    Remove spaces between "print" and "()"

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -8,8 +8,8 @@
 import random
 
 # Intro
-print ("""Welcome to 'Guess the Number'! Guess the number between 1 and 10, and you win!""")
-print ()
+print("""Welcome to 'Guess the Number'! Guess the number between 1 and 10, and you win!""")
+print()
 
 # Set up the iterator
 guessCount = 3
@@ -23,37 +23,39 @@ r = random.randint(1,11)
 # While the number of guesses is greater than zero
 while guessCount > 0:
     if guessCount == 1:
-        print ("You have " + str(guessCount) + " guess remaining.")
+        print("You have " + str(guessCount) + " guess remaining.")
     else:
-        print ("You have " + str(guessCount) + " guesses remaining.")
+        print("You have " + str(guessCount) + " guesses remaining.")
     guess = int(input("What number do you guess? "))
     # Check if the guess was correct
     if guess == r:
-        print ("Congratulations! You win!")
+        print("Congratulations! You win!")
         break
     else:
         # Guess is too high
         if guess > r and guess < 11:
             print ("Your guess was too high.")
             guessCount -= 1
-            print ()
+            print()
             # Out of tries
             if guessCount == 0:
-                print ("Game Over")
+                print("The number was " + str(r) + ".")
+                print("Game Over")
                 break
             continue
         # Guess is too low
         elif guess < r and guess > 0:
-            print ("Your guess was too low.")
+            print("Your guess was too low.")
             guessCount -= 1
-            print ()
+            print()
             # Out of tries
             if guessCount == 0:
-                print ("Game Over")
+                print("The number was " + str(r) + ".")
+                print("Game Over")
                 break
             continue
         else:
-            print ("Sorry, your guess wasn't a number between 1 and 10.")
+            print("Sorry, your guess wasn't a number between 1 and 10.")
             guessCount -= 1
-            print ()
+            print()
             continue
